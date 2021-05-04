320c322186242d87d4a85a49cd11a6e71bf45a93
Eli Semory <elisemory@snap.com>
2021-05-04 16:55:05 -0400

    hunterize filament

--- libs/filamentapp/src/IBL.cpp
+++ libs/filamentapp/src/IBL.cpp
@@ -26,7 +26,11 @@
 #include <image/KtxBundle.h>
 #include <image/KtxUtility.h>
 
+#ifdef FILAMENT_USE_HUNTER
+#include <stb/stb_image.h>
+#else
 #include <stb_image.h>
+#endif
 
 #include <utils/Path.h>
 
