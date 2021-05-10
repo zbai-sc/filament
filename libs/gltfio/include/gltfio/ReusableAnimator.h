99b33109680ee6a10a345b4ea95d8c04541c05b2
Bryan Bai <zbai@snapchat.com>
2021-05-10 03:22:33 -0400

    Add CPU morpher
    - unlimited number of morph targets can be activated at the same time
    - beware of performance penalty as we need to upload buffer every frame
    - can be cached and reused

--- libs/gltfio/include/gltfio/ReusableAnimator.h
+++ libs/gltfio/include/gltfio/ReusableAnimator.h
@@ -23,6 +23,7 @@
 namespace gltfio {
 
 struct ReusableAnimatorImpl;
+class CPUMorpher;
 
 /**
  * \class ReusableAnimator ReusableAnimator.h gltfio/ReusableAnimator.h
@@ -72,8 +73,19 @@ public:
     /** Remove the model to be animated. */
     void removeAnimatedAsset();
 
-    /** Add the model to be animated. */
-    void addAnimatedAsset(FilamentAsset* assetToAnimate);
+    /** Add the model to be animated. 
+     * 
+     * NOTE: by default ReusableAnimator creates and owns an internal morpher. i.e. 
+     * ReusableAnimator will copy and destroy the morpher when apporiate. But if an external 
+     * morpher is supplied here, ReusableAnimator will not create the internal morpher anymore.
+    */
+    void addAnimatedAsset(FilamentAsset* assetToAnimate, CPUMorpher* morpher = nullptr);
+
+    /** Create a morpher that can be reused later. */
+    static CPUMorpher* createMorpher(FilamentAsset* asset);
+
+    /** Destroy morpher. */
+    static void destroyMorpher(CPUMorpher* morpher);
 
 private:
     
